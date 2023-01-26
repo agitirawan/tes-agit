0fa3a659edabe6861b90763a54e70cf3907020aa
agit ari irawan <agitari17@gmail.com>
2023-01-26 07:35:20 +0700

    update controller and view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://icons.getbootstrap.com/assets/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>VueJs Tes</title>
   </head>
   <body>
     
